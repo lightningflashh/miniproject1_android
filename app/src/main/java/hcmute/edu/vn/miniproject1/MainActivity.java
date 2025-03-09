02793d5357d6dc848ddc1be29415bc22bef65f7d
lightningflashh <thanhchi0429@gmail.com>
2025-03-09 09:48:55 +0700

    feat: Implement song playback notification and service
    This commit introduces a new feature that allows song playback to continue in the background via a foreground service and a custom notification. The changes include:
    
    -   **New layout for custom notifications:**
        -   `layout_custom_notification.xml`: Defines the layout for the custom notification, including the song image, title, artist, play/pause button, and clear button.
    -   **Constants:**
        -   `Constants.java`: Adds constants for actions, channel ID, and other service-related information.
    -   **AndroidManifest.xml:**
        -   Adds permissions for foreground service, media playback, and post notifications.
        -   Declares `SongService` as a foreground service.
        -   Declares `SongReceiver` to handle boot completed events.
        -   Adds `ItemSongActivity` and makes `ListSongActivity` be launcher
        -   Adds `MyApplication` to create notification channels
    -   **MyApplication.java:**
        -   Implements the creation of a notification channel for the foreground service.
    -   **ic_notification:**
        -   Adds ic notification
    -   **ListSongActivity.java:**
        -   Adds broadcast receiver.
        -   Adds actions for play, pause, resume, clear, and next.
        -   Adds a music bar to display current song information.
        -   Adds functions to start, stop, and control the song service.
        -   Add `mapping()` function to assign ids
        -   Refactor `clickStartService`, `onClickGoToPlaySong`, `clickStopService`
        -   Add `onClickNext()` to change next song
    -   **activity_list_song.xml:**
        -   Adds a music bar layout at the bottom to control and display song information.
        -   Reorganizes the layout to accommodate the music bar.
    -   **SongReceiver.java:**
        -   Implements a broadcast receiver to handle song actions.
    -   **SongService.java:**
        -   Implements a foreground service to handle song playback.
        -   Includes logic for starting, pausing, resuming, and clearing music.
        -   Implements a notification to control playback.
        -   Includes functions to send notification and handle actions.
    -   **ic_clear.png:**
        -   Adds clear icon
    - `.idea/deploymentTargetSelector.xml`
      - select target device
    These changes enhance the user experience by allowing music to play in the background and be controlled through a notification.

--- app/src/main/java/hcmute/edu/vn/miniproject1/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/miniproject1/MainActivity.java
@@ -1,6 +1,8 @@
 package hcmute.edu.vn.miniproject1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import hcmute.edu.vn.miniproject1.controllers.ListSongActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlayMusic, btnSMSAndCall, btnAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlayMusic = findViewById(R.id.btn_play_music);
+        btnSMSAndCall = findViewById(R.id.btn_sms_call);
+        btnAlarm = findViewById(R.id.btn_alarm);
+
+        btnPlayMusic.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ListSongActivity.class);
+            startActivity(intent);
+        });
     }
 }
